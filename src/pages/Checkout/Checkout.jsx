1aec1659255eebd286055c479f7f25f79cbe35df
Inna Batalova <batalovainna97@gmail.com>
2023-09-27 12:20:20 +0300

    Create: added checkout and media

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Breadcrumb from '../../components/Breadcrumb/Breadcrumb'
 import CheckoutInfoItem from '../../components/CheckoutInfoItem/CheckoutInfoItem'
 import CheckoutInfoProduct from '../../components/CheckoutInfoProduct/CheckoutInfoProduct'
+import FormField from '../../components/PagesComp/FormField/FormField'
+import Button from '../../components/DesignComponents/Button'
 
 const Checkout = () => {
   const breadcrumbListItemProps = [
@@ -20,6 +22,11 @@ const Checkout = () => {
       priceCheckoutInfoProductProps={item[1]} />
   )
 
+  const [activeTab, setActiveTab] = useState('active-tab-1')
+
+  const [checkedDelivery, setCheckedDelivery] = useState('postRussia')
+  const [checkedPayment, setCheckedPayment] = useState('paymentCard')
+
   return (
     <div className="checkout">
       <div className="grid-container">
@@ -28,7 +35,115 @@ const Checkout = () => {
 
         <div className="checkout-content">
 
-          <div className="checkout-payment"></div>
+          <div className="checkout-payment">
+
+            <div className="checkout-payment__comp">
+              <div className='checkout-payment__comp__top'>
+                <span className={`checkout-payment__comp__top__title ` + (activeTab === 'active-tab-1' ? 'checkout-payment__comp__top__title_active' : '')}>Авторизован как batalovainna97@gmail.com</span>
+                <span className={`checkout-payment__comp__top__link ` + (activeTab === 'active-tab-1' ? 'checkout-payment__comp__top__link_active' : '')}
+                onClick={() => setActiveTab('active-tab-1')}>Изменить</span>
+              </div>
+              <div className={`checkout-payment__comp__bottom ` + (activeTab === 'active-tab-1' ? 'checkout-payment__comp__bottom_active' : '')}>
+                <FormField titleFormFieldProps='Телефон' typeFormFieldProps='phone'
+                  nameFormFieldProps='phone' classFormFieldProps='checkout-payment__comp__field' />
+                <span className="checkout-payment__comp__auth">Авторизоваться как другой пользователь</span>
+                <Button classDesignButtonProps='orange' classSizeButtonProps='45' classButtonProps='checkout-payment__comp__button' 
+                  titleButtonProps='Продолжить' clickDesignButtonProps={() => setActiveTab('active-tab-2')}/>
+              </div>
+            </div>
+
+            <div className="checkout-payment__comp">
+              <div className={`checkout-payment__comp__top ` + (activeTab === 'active-tab-2' ? 'checkout-payment__comp__top_active' : '')}>
+                <span className={`checkout-payment__comp__top__title ` + (activeTab === 'active-tab-2' ? 'checkout-payment__comp__top__title_active' : '')}>Адрес доставки</span>
+                <span className={`checkout-payment__comp__top__link ` + (activeTab === 'active-tab-2' ? 'checkout-payment__comp__top__link_active' : '')}
+                onClick={() => setActiveTab('active-tab-2')}>Изменить</span>
+              </div>
+              <div className={`checkout-payment__comp__bottom ` + (activeTab === 'active-tab-2' ? 'checkout-payment__comp__bottom_active' : '')}>
+                <div className="profiles-user__wrapprof checkout-payment__comp__wrapprof">
+                  <a className="profiles-user__linkprof">Основной</a>
+                  <a className="profiles-user__overprof checkout-payment__comp__overprof">Новый тестовый</a>
+                  <span>или</span>
+                  <a className="profiles-user__linkprof">Добавить новый адрес доставки</a>
+                </div>
+                <div className="checkout-payment__comp__form">
+                  <div className="checkout-payment__comp__wrapfield">
+                    <FormField titleFormFieldProps='Имя' typeFormFieldProps='name'
+                      nameFormFieldProps='name' classFormFieldProps='checkout-payment__comp__field profiles-user__field' />
+                  </div>
+                  <div className="checkout-payment__comp__wrapfield">
+                    <FormField titleFormFieldProps='Фамилия' typeFormFieldProps='surname'
+                      nameFormFieldProps='surname' classFormFieldProps='checkout-payment__comp__field' />
+                  </div>
+                  <div className="checkout-payment__comp__wrapfield">
+                    <FormField titleFormFieldProps='E-mail' typeFormFieldProps='email'
+                      nameFormFieldProps='Email' classFormFieldProps='checkout-payment__comp__field' />
+                  </div>
+                  <div className="checkout-payment__comp__wrapfield">
+                    <FormField titleFormFieldProps='Телефон' typeFormFieldProps='phone'
+                      nameFormFieldProps='phone' classFormFieldProps='checkout-payment__comp__field' />
+                  </div>
+                  <div className="checkout-payment__comp__wrapfield checkout-payment__comp__wrapfield_address">
+                    <FormField titleFormFieldProps='Адрес доставки' typeFormFieldProps='address'
+                      nameFormFieldProps='address' classFormFieldProps='checkout-payment__comp__field' />
+                  </div>
+                  <div className="checkout-payment__comp__wrapfield">
+                    <FormField titleFormFieldProps='Город' typeFormFieldProps='city'
+                      nameFormFieldProps='city' classFormFieldProps='checkout-payment__comp__field' />
+                  </div>
+                  <div className="checkout-payment__comp__wrapfield">
+                    <FormField titleFormFieldProps='Почтовый индекс' typeFormFieldProps='code'
+                      nameFormFieldProps='code' classFormFieldProps='checkout-payment__comp__field' />
+                  </div>
+                </div>
+                <Button classDesignButtonProps='orange' classSizeButtonProps='45' classButtonProps='checkout-payment__comp__button' 
+                  titleButtonProps='Продолжить' clickDesignButtonProps={() => setActiveTab('active-tab-3')}/>
+              </div>
+            </div>
+
+            <div className="checkout-payment__comp">
+              <div className={`checkout-payment__comp__top ` + (activeTab === 'active-tab-3' ? 'checkout-payment__comp__top_active' : '')}>
+                <span className={`checkout-payment__comp__top__title ` + (activeTab === 'active-tab-3' ? 'checkout-payment__comp__top__title_active' : '')}>Выбор способа доставки</span>
+                <span className={`checkout-payment__comp__top__link ` + (activeTab === 'active-tab-3' ? 'checkout-payment__comp__top__link_active' : '')}
+                onClick={() => setActiveTab('active-tab-3')}>Изменить</span>
+              </div>
+              <div className={`checkout-payment__comp__bottom ` + (activeTab === 'active-tab-3' ? 'checkout-payment__comp__bottom_active' : '')}>
+                <div className="checkout-payment__comp__checkbox" onClick={() => setCheckedDelivery('postRussia')}>
+                  <input type="checkbox" className="bort-checkbox filter-checkbox" id="postRussia" checked={checkedDelivery == 'postRussia' ? true : false}/>
+                  <label for="postRussia" className="bort-checkbox-label filter-checkbox-label">Почта России. Бесплатная доставка</label>
+                </div>
+                <div className="checkout-payment__comp__checkbox" onClick={() => setCheckedDelivery('DPD')}>
+                  <input type="checkbox" className="bort-checkbox filter-checkbox" id="DPD" checked={checkedDelivery == 'DPD' ? true : false} />
+                  <label for="DPD" className="bort-checkbox-label filter-checkbox-label">Курьером до двери по России. Бесплатная доставка</label>
+                </div>
+                <span className="checkout-payment__comp__result">Итого: 0 Р</span>
+                <Button classDesignButtonProps='orange' classSizeButtonProps='45' classButtonProps='checkout-payment__comp__button' 
+                  titleButtonProps='Продолжить' clickDesignButtonProps={() => setActiveTab('active-tab-4')}/>
+              </div>
+            </div>
+
+            <div className="checkout-payment__comp">
+              <div className={`checkout-payment__comp__top ` + (activeTab === 'active-tab-4' ? 'checkout-payment__comp__top_active' : '')}>
+                <span className={`checkout-payment__comp__top__title ` + (activeTab === 'active-tab-4' ? 'checkout-payment__comp__top__title_active' : '')}>Выбор варианта оплаты</span>
+                <span className={`checkout-payment__comp__top__link ` + (activeTab === 'active-tab-4' ? 'checkout-payment__comp__top__link_active' : '')} 
+                onClick={() => setActiveTab('active-tab-4')}>Изменить</span>
+              </div>
+              <div className={`checkout-payment__comp__bottom ` + (activeTab === 'active-tab-4' ? 'checkout-payment__comp__bottom_active' : '')}>
+                <div className="checkout-payment__comp__checkbox" onClick={() => setCheckedPayment('paymentCard')}>
+                  <input type="checkbox" className="bort-checkbox filter-checkbox" id="paymentCard" checked={checkedPayment == 'paymentCard' ? true : false} />
+                  <label for="paymentCard" className="bort-checkbox-label filter-checkbox-label">Оплата картой онлайн</label>
+                </div>
+                <div className="checkout-payment__comp__checkbox" onClick={() => setCheckedPayment('cashlessPay')}>
+                  <input type="checkbox" className="bort-checkbox filter-checkbox" id="cashlessPay" checked={checkedPayment == 'cashlessPay' ? true : false} />
+                  <label for="cashlessPay" className="bort-checkbox-label filter-checkbox-label">Безналичный расчет для юридических лиц</label>
+                </div>
+                <span class="bort-modal-text authorization-form-text checkout-payment__comp__label">Ваш комментарий к заказу</span>
+                <textarea className="checkout-payment__comp__textarea"  name="comment" id="comment" cols="30" rows="10"></textarea>
+                <Button classDesignButtonProps='orange' classSizeButtonProps='45' classButtonProps='checkout-payment__comp__button' 
+                  titleButtonProps='Выбор варианта оплаты' clickDesignButtonProps={() => setActiveTab('active-tab-1')}/>
+              </div>
+            </div>
+
+          </div>
 
           <div className="checkout-info">
             <CheckoutInfoItem titleCheckoutInfoItemProps='Не получается оформить заказ?'
@@ -36,7 +151,7 @@ const Checkout = () => {
                 <span className="checkout-info__item__text">
                   <a href="#" className="checkout-info__item__link">Напишите нам</a>, мы обязательно поможем
                 </span>
-              } activeClassCheckoutInfoItemProps=''/>
+              } activeClassCheckoutInfoItemProps='' />
             <CheckoutInfoItem titleCheckoutInfoItemProps='Промо-код'
               infoCheckoutInfoItemProps={
                 <>
@@ -52,10 +167,10 @@ const Checkout = () => {
                     <span>15 230 ₽</span>
                   </div>
                 </>
-              } activeClassCheckoutInfoItemProps='_active'/>
+              } activeClassCheckoutInfoItemProps='_active' />
             <CheckoutInfoItem titleCheckoutInfoItemProps='Товаров в вашем заказе'
-              infoCheckoutInfoItemProps={sortCheckoutInfoProduct} 
-              activeClassCheckoutInfoItemProps=''/>
+              infoCheckoutInfoItemProps={sortCheckoutInfoProduct}
+              activeClassCheckoutInfoItemProps='' />
             <CheckoutInfoItem titleCheckoutInfoItemProps='Информация о заказе'
               infoCheckoutInfoItemProps={
                 <>
@@ -72,7 +187,7 @@ const Checkout = () => {
                     <li>Почта России</li>
                   </ul>
                 </>
-              } activeClassCheckoutInfoItemProps=''/>
+              } activeClassCheckoutInfoItemProps='' />
           </div>
 
         </div>
